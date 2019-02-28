fc0c1e3c598970c462d5f67f5796cd1ea8efa919
jwchan1996 <741755613@qq.com>
2019-03-01 00:36:56 +0800

    抽离幻灯片组件

---
+++ components/slide-swiper/slide-swiper.js
@@ -0,0 +1,32 @@
+// components/slide-swiper/slide-swiper.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgUrls: [
+      'http://wx4.sinaimg.cn/mw690/0060lm7Tly1fz3nlb1njaj30u016gn54.jpg','https://gss1.bdstatic.com/9vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike272%2C5%2C5%2C272%2C90/sign=cbb11d8401d79123f4ed9c26cc5d32e7/b8389b504fc2d56284049d61ea1190ef76c66c29.jpg',
+      'http://wx2.sinaimg.cn/mw690/0060lm7Tly1fz4upw54j4j30ch0hsdh7.jpg'
+    ],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    circular: false,
+    interval: 3000,
+    duration: 800,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
