09666882e520234561e787eff4b7f8f03cba4695
jwchan1996 <741755613@qq.com>
2019-03-01 00:37:47 +0800

    规范组件引入名称

--- pages/square/square.js
+++ pages/square/square.js
@@ -5,27 +5,13 @@ const app = getApp()
 const navigationBarHeight = (app.statusBarHeight + 44) + 'px'
 
 Page({
-
-  inputValue: '',
   /**
    * 页面的初始数据
    */
   data: {
     //导航条标题
-    // navigationBarTitle: '情之林',
     navigationBarTitle: '热门新番',
     navigationBarHeight: navigationBarHeight,
-    imgUrls: [
-      'http://wx4.sinaimg.cn/mw690/0060lm7Tly1fz3nlb1njaj30u016gn54.jpg',
-      'https://gss1.bdstatic.com/9vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike272%2C5%2C5%2C272%2C90/sign=cbb11d8401d79123f4ed9c26cc5d32e7/b8389b504fc2d56284049d61ea1190ef76c66c29.jpg',
-      'http://wx2.sinaimg.cn/mw690/0060lm7Tly1fz4upw54j4j30ch0hsdh7.jpg'
-    ],
-    indicatorDots: true,
-    vertical: false,
-    autoplay: true,
-    circular: false,
-    interval: 3000,
-    duration: 800,
   },
 
   /**
