2f42a8ba487c8d8924877641eee992d0a3b69eab
jwchan1996 <741755613@qq.com>
2019-03-01 19:55:51 +0800

    日常提交

--- pages/square/square.js
+++ pages/square/square.js
@@ -17,56 +17,68 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function (res) {
-    
+  onReady(res) {
+    this.getRecommendList()
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage() {
 
   },
+
+  /**
+   * 获取番剧推荐
+   */
+  getRecommendList() {
+    wx.request({
+      url: 'https://api.clicli.top/posts/both?status=public&type=tuijian&page=1&pageSize=20',
+      success(res) {
+        console.log(res.data)
+      }
+    })
+  }
 })
